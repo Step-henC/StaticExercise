e56e1edca66e0d449dce266546557298fd5de65a
Stephen Cunningham <stevecunningham@outlook.com>
2022-03-24 17:06:24 -0500

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter temperature in Fahrenheit to nearest tenths place:");
+            double fahrenTemp = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("The temperature in Celsius is: ");
+            Console.WriteLine(TempConverter.FahrenheitToCelsius(fahrenTemp));
+
+            Console.WriteLine("Please enter temperature in Celsius to nearest tenths place: ");
+            double celsTemp = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("The temperature in Fahrenheit is: ");
+            Console.WriteLine(TempConverter.CelsiusToFahrenheit(celsTemp));
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        /*one called FahrenheitToCelsius that will require a double as a 
+            parameter and return a double, the other CelsiusToFahrenheit 
+            which will also require a double as a parameter and return a double. 
+            Fill out these methods and call them in your Program.cs file.
+            Make sure they’re accurate!*/
+        public static double FahrenheitToCelsius(double fahrtemp)
+        {
+            return Math.Round(((fahrtemp - 32.0) * .55), 2);
+        }
+        public static double CelsiusToFahrenheit(double celsTemp)
+        {
+            return Math.Round((celsTemp * 1.8 + 32.0), 2);
+        }
+    }
+}
